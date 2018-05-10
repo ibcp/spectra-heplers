ad7e87ae27106db45d4004f9868a6584269b5e33
Rustam Guliev <glvrst@gmail.com>
2018-05-10 15:39:37 +0300

    Formalize normalize.R and straightline.R

--- normalize.R
+++ normalize.R
@@ -1,39 +1,91 @@
-normalize <- function (spc, type = c('scale', 'area', 'vector', 'minmax', 'max', 'peak'), 
-            peak.range = wl2i(spc, min~max), 
-            scale.options = list(center = TRUE, scale = TRUE)) {
+#' Normalize spectra using different type of normalizations
+#'
+#' @param x - spectra data in one of formats: data.frame, matrix, numeric vector or a hyperSpec object. If data.frame or matrix - one row is one spectrum. 
+#' @param type - type of normalization. 
+#' `area` - makes area of each spectrum to be equal 1. 
+#' `vector` - vector normalization, makes vector norm (i.e. sqrt(sum(\code{x}^2))) of each spectrum to be equal 1 
+#' `minmax` - works the same as hyperSpec::normalize01: the values of a spectrum are mapped to [0, 1] by subtracting the minimum and subsequently dividing by the maximum. If all elements of \code{x} are equal, 1 is returned.
+#' `max` - devides all values of a spectrum to max, i.e. makes maximum value to be qual 1.
+#' `peak` - devides all values of a spectrum to a peak value, i.e. makes value of a peak to be qual 1. The value of peak is calculated as max value within a range of the peak defined by peak.range argument.  
+#' @param peak.range - vector of two values defining range of peak, i.e. like \code{c(1500, 1600)}.
+#' @param peak.index - logical. By default values in peak.range are related to colnames for data.frame and matrix, names for a vector, and wavelength for a hyperSpec object. However, indexes can be use by setting this argument to TRUE. 
+#' @param na.rm - logical. Should missing values (including NaN) be removed?
+#' @param tolerance tolerance level for determining what is 0 and 1 for 'minmax' normalization
+#' @return Returns the normalized values in the format used in \code{x}, i.e. data.frame, matrix, hyperSpec or numeric vector. 
+#' @author Rustam Guliev <glvrst@gmail.com>
+#' @examples 
+#' # Load test data from hyperSpec
+#' library(hyperSpec)
+#' flu.normalized <- normalize(flu, 'area')
+#' flu.normalized <- normalize(flu, 'area')
+#' laser.normalized <- normalize(laser, 'peak', peak.range=c(404,405.15))
+
+normalize <- function (x, type = c('area', 'vector', 'minmax', 'max', 'peak'), 
+                       peak.range = NULL, peak.index = FALSE, 
+                       na.rm = TRUE, tolerance = hy.getOption ("tolerance")) {
+  # Check class of X and make it a matrix
+  if (is.data.frame(x)) {
+    spc <- as.matrix(x)
+  } else if (is.vector(x) && is.numeric(x)) {
+    spc <- matrix(x, nrow = 1)
+    colnames(spc) <- names(x)
+  } else if (is (x,"hyperSpec")) {
+    spc <- x$spc
+    colnames(spc) <- wl(x)
+  } else if (is.matrix(x)) {
+    spc <- x
+  } else {
+    stop('Icorrect format of data. X must be one of following: "data.frame", "matrix", "numeric vector" or a "hyperSpec" object')
+  }
+  
+  # Check type is correct. And peak.range is set if type is 'peak'
+  type <- match.arg(type)
+  
   switch(
     type,
-      scale = {
-        new.spc <- scale(spc, center = scale.options$center, scale = scale.options$scale)
-      },
-      area = {
-        m <- apply(spc, 1, mean, na.rm = TRUE)
-        new.spc <- sweep (spc, 1, m, "/")
-      },
-      vector = {
-        norm.to <- apply(spc, 1, function (x) {sqrt(sum(x[!is.na(x)]*x[!is.na(x)]))})
-        new.spc <- sweep (spc, 1, norm.to, "/")
-      },
-      minmax = {
-        m <- apply(spc, 1, min, na.rm = TRUE)
-        new.spc <- sweep (spc, 1, m, `-`)
-        m <- apply(new.spc, 1, max, na.rm = TRUE)
-        new.spc <- sweep (new.spc, 1, m, `/`)
-      },
-      max = {
-        m <- apply(spc, 1, function(x) {max(abs(x),na.rm = TRUE)})
-        new.spc <- sweep (spc, 1, m, `/`)
-      },
-      peak = {
-        if (missing(peak.range)) {
-          stop('wl.range must be setted when peak normalization is used!')
-        }
-        norm.to <- apply(spc[,, peak.range, wl.index = TRUE], 1, max, na.rm = TRUE)
-        new.spc <- sweep (spc, 1, norm.to, "/")
-      },
-      {
-        stop('Unknown type of normalization!')
+    area = {
+      m <- apply(spc, 1, mean, na.rm = na.rm)
+      new.spc <- sweep (spc, 1, m, "/")
+    },
+    vector = {
+      norm.to <- apply(spc, 1, function (x) {sqrt(sum(x*x, na.rm = na.rm))})
+      new.spc <- sweep (spc, 1, norm.to, "/")
+    },
+    minmax = {
+      m <- apply(spc, 1, min, na.rm = na.rm)
+      new.spc <- sweep (spc, 1, m, `-`)
+      m <- apply(new.spc, 1, max, na.rm = na.rm)
+      new.spc <- sweep (new.spc, 1, m, `/`)
+      new.spc [m < tolerance, ] <- 1
+    },
+    max = {
+      m <- apply(spc, 1, function(x) {max(abs(x),na.rm = na.rm)})
+      new.spc <- sweep (spc, 1, m, `/`)
+    },
+    peak = {
+      stopifnot(is.vector(peak.range) && length(peak.range) == 2)
+      if (peak.index) {
+        pkrange <- min(peak.range):max(peak.range)
+      } else {
+        clnames <- as.numeric(colnames(spc))
+        pkrange <- clnames >= min(peak.range) & clnames <= max(peak.range) 
       }
+      norm.to <- apply(spc[, pkrange], 1, max, na.rm = na.rm)
+      new.spc <- sweep (spc, 1, norm.to, "/")
+    },
+    {
+      stop('Unknown type of normalization!')
+    }
   )
-  return(new.spc)
+  
+  # Make output to have the same type as input x.
+  if (is.data.frame(x)) {
+    return(as.data.frame(new.spc))
+  } else if (is.vector(x) && is.numeric(x)) {
+    return(new.spc[1,])
+  } else if (is (x,"hyperSpec")) {
+    x$spc <- new.spc
+    return(x)
+  }
+  return(new.spc) 
 }
--- straightline.R
+++ straightline.R
@@ -1,15 +1,94 @@
-straightline <- 
-  function(spc, wl.left, wl.right) {
-    indx.left <- wl2i(s,  wl.left)[1]
-    indx.right <- wl2i(s, wl.right)[1]
-    x <- c(wl(s)[indx.left], wl(s)[indx.right])
-    
-    for (i in 1:nrow(s)){
-      y <- s$spc[i,c(indx.left, indx.right)]
-      k <- (y[2]-y[1])/(x[2]-x[1])
-      b <- (x[2]*y[1]-x[1]*y[2])/(x[2]-x[1])
-      s$spc[i,indx.left:indx.right] <- wl(s)[indx.left:indx.right]*k + b
+#' Generate straight line between two points
+#' 
+#' The values in given data are replaced by straight line. 
+#' This is one of ways that cosmic particles cotribution in Raman/SERS spectra can be handled.
+#'
+#' @param data - spectra data in one of formats: data.frame, matrix, numeric vector or a hyperSpec object. If data.frame or matrix - one row is one spectrum.
+#' @param x - vector of x axis values. By defauld colnames/names used.
+#' @param from - defines x value of left side of the range to be strainghtlined. 
+#' @param to - defines x value of right side of the range to be strainghtlined.
+#' @param index - logical. Should values \code{from} and \code{to} to be interpreted as indexes? 
+#' @return Matrix with values replaced with a strainght line.
+#' @author Rustam Guliev <glvrst@gmail.com>
+#' @examples 
+#' # Load test data from hyperSpec
+#' library(hyperSpec)
+#' 
+#' # Pretend we need to replace range between 900 and 1200 for the first ten spectra.
+#' res <- straightline(chondro[1:10], from = 990, to = 1010)
+#' plotspc(res[,,900~1100])
+
+straightline <- function(data, x = NULL, from = NULL, to = NULL, index = FALSE) {
+  # Define x axis values
+  if (is.null(x)) {
+    if (is.data.frame(data) || is.matrix(data)) {
+      x <- as.numeric(colnames(data))
+    } else if (is.vector(data) && is.numeric(data)) {
+      x <- as.numeric(names(data))
+    } else if (is (data,"hyperSpec")) {
+      x <- wl(data)
+    } else {
+      stop('Icorrect format of data. DATA must be one of following: "data.frame", "matrix", "numeric vector" or a "hyperSpec" object')
     }
-    
-    return(s)
   }
+  
+  # Define the range to be replaced by straight line
+  # This is simply defining indexes of "left" and "right" points of the line.
+  # cols - defines columns to be changed. Usualy they are just columns between 'left' and 'right' indexed,
+  # but we make it for flexible in case columns are not ordered.
+  if (index) {
+    if (is.null(from)) {
+      from <- 1
+    }
+    if (is.null(to)) {
+      to <- length(x)
+    }
+    indx.left <- from
+    indx.right <- to
+    cols <- from:to
+  } else {
+    if (is.null(from)) {
+      from <- min(x)
+    }
+    if (is.null(to)) {
+      to <- max(x)
+    }
+    indx.left <- which(x == from)
+    if (length(indx.left) == 0) {
+      stop(paste0('No such wavelength "', from, '". Use existing wavelength for from and to parameters'))
+    }
+    indx.right <- which(x == to)
+    if (length(indx.right) == 0) {
+      stop(paste0('No such wavelength "', to, '". Use existing wavelength for from and to parameters'))
+    }    
+    cols <- (x >= from) & (x <= to)
+  }
+   
+  # Use formula y = k*x + b to generate a straight line. 
+  # Having two points (left and right) (x1,y1) and (x2,y2), k = (y2-y1)/(x2-x1) and b = (x2*y1 - x1*y2)/(x2-x1)
+  x1 <- x[indx.left]
+  x2 <- x[indx.right]
+  if (is (data,"hyperSpec")) {
+    y1 <- data$spc[,indx.left]
+    y2 <- data$spc[,indx.right]
+  } else if (is.vector(data)) {
+   y1 <- data[indx.left]
+   y2 <- data[indx.right]
+  } else {
+   y1 <- data[,indx.left]
+   y2 <- data[,indx.right]
+  }
+
+  k <- matrix((y2-y1) / (x2-x1), ncol=1)
+  b <- (x2*y1 - x1*y2) / (x2-x1)
+  
+  if (is (data,"hyperSpec")) {
+    data$spc[,cols] <- k %*% x[cols] + b
+  } else if (is.vector(data)) {
+    data[cols] <- k[,1] * x[cols] + b
+  } else {
+    data[,cols] <- k %*% x[cols] + b
+  }
+  return(data)
+}
+
